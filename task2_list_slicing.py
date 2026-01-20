2e20b90c42ead4628ba10f8707dcbd3dd8c82088
Dibyeshbanerjee <105985392+Dibyeshbanerjee@users.noreply.github.com>
2026-01-20 20:04:27 +0530

    Create task2_list_slicing.py

---
+++ task2_list_slicing.py
@@ -0,0 +1,9 @@
+# Task 2: Demonstrate List Slicing
+
+numbers = list(range(1, 11))
+
+first_five = numbers[:5]
+reversed_list = first_five[::-1]
+
+print("First five elements:", first_five)
+print("Reversed list:", reversed_list)
